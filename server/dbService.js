e62ad229a2239bd62f736667bfacb664c002579f
Manuela Ienuso <43219535+emmegray@users.noreply.github.com>
2022-12-20 13:39:38 +0100

    Connect the db

--- server/dbService.js
+++ server/dbService.js
@@ -4,8 +4,15 @@ dotenv.config();
 
 const connection = mysql.createConnection({
   host: process.env.HOST,
-  user: process.env.USERNAME,
+  user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.DB_PORT
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log('db' + connection.state);
 })
